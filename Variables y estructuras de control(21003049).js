c26b5a8aaf4518adbf8273e0bff8f902cf426858
YendiCorado <yendi.corado@galileo.edu>
2022-02-12 20:15:03 -0600

    Create Variables y estructuras de control(21003049).js

---
+++ Variables y estructuras de control(21003049).js
@@ -0,0 +1,19 @@
+var mes = 10;
+var estacion;
+
+if (mes == 1 || mes == 2 || mes == 12) (
+estacion = "Invierno"
+)
+  else if (mes == 3 || mes == 4 || mes == 5)(
+    estacion = "Primavera"
+  )
+    else if (mes == 6 || mes == 7 || mes == 8)(
+      estacion = "Verano"
+    )
+      else if (mes == 9 || mes == 10 || mes == 11)(
+        estacion = "Otonio"
+      )
+        
+console.log (estacion)
+console.log ("Nombre: YENDI MISHEL IPINIA CORADO")
+console.log("Carnet: 21003049")
